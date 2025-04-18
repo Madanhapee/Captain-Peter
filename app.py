bf6133aaea811f5ddcf7c926e6b520ece6f33a37
Madanhapee <150229338+Madanhapee@users.noreply.github.com>
2025-04-18 18:00:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import os
 from datetime import datetime, timedelta, timezone
 from sklearn.preprocessing import LabelEncoder
 from supabase import create_client, Client
-import httpx  # Import httpx
+import httpx  
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=4)
-# Initialize Flask-Login
+# Initializing Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
